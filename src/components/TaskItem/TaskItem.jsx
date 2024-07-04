997dd062c903cc745452752dfc2f03db156efde2
minhajshafiq <minhaj93500@gmail.com>
2024-07-04 11:42:18 +0200

    Interactivité ajouté

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./taskItem.css";
 
 function TaskItem({ text, checked }) {
+  const [isChecked, setIsChecked] = useState(checked);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div className="task_item">
-      <input type="checkbox" checked={checked} readOnly />
-      <p className='task_itemText'>{text}</p>
+      <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+      <p className="task_itemText">{text}</p>
     </div>
   );
 }
